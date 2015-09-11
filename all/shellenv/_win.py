32d7921de5768fd74983ebff6fa37212aed24e83
wbond <will@wbond.net>
2015-09-11 13:41:17 -0400

    Use kernel32 with ST2 on Windows to get unicode environmental variable values

--- all/shellenv/_win.py
+++ all/shellenv/_win.py
@@ -3,11 +3,18 @@ from __future__ import unicode_literals, division, absolute_import, print_functi
 
 import os
 import locale
+import sys
+import ctypes
 
 from ._types import str_cls
 
 
 _sys_encoding = locale.getpreferredencoding()
+
+kernel32 = ctypes.windll.kernel32
+
+kernel32.GetEnvironmentStringsW.argtypes = []
+kernel32.GetEnvironmentStringsW.restype = ctypes.c_void_p
 
 
 def get_env(shell=None):
@@ -27,4 +34,21 @@ def get_env(shell=None):
     if not isinstance(shell, str_cls):
         shell = shell.decode(_sys_encoding)
 
-    return (shell, dict(os.environ))
+    if sys.version_info < (3,):
+        str_pointer = kernel32.GetEnvironmentStringsW()
+        string = ctypes.wstring_at(str_pointer)
+
+        values = {}
+        while string != '':
+            if string[0].isalpha():
+                name, value = string.split(u'=', 1)
+                values[name] = value
+            # Include the trailing null byte, and measure each
+            # char as 2 bytes since Windows uses UTF-16 for
+            # wide chars
+            str_pointer += (len(string) + 1) * 2
+            string = ctypes.wstring_at(str_pointer)
+    else:
+        values = dict(os.environ)
+
+    return (shell, values)
