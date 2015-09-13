fe53ccd23ec0dbb2542886ab94dc2afdaeabb8ff
wbond <will@wbond.net>
2015-09-13 13:20:39 -0400

    Handle CoreFoundation issue with ST2 on OS X in fetching user name string

--- all/shellenv/_osx/core_foundation.py
+++ all/shellenv/_osx/core_foundation.py
@@ -28,6 +28,9 @@ CFTypeRef = POINTER(CFType)
 CoreFoundation.CFStringGetCStringPtr.argtypes = [CFStringRef, CFStringEncoding]
 CoreFoundation.CFStringGetCStringPtr.restype = c_char_p
 
+CoreFoundation.CFStringGetCString.argtypes = [CFStringRef, c_char_p, CFIndex, CFStringEncoding]
+CoreFoundation.CFStringGetCString.restype = ctypes.c_bool
+
 CoreFoundation.CFStringCreateWithCString.argtypes = [CFAllocatorRef, c_char_p, CFStringEncoding]
 CoreFoundation.CFStringCreateWithCString.restype = CFStringRef
 
@@ -63,6 +66,17 @@ def cfstring_to_unicode(value):
         ctypes.cast(value, CFStringRef),
         kCFStringEncodingUTF8
     )
+    if string is None:
+        buf = ctypes.create_string_buffer(1024)
+        result = CoreFoundation.CFStringGetCString(
+            ctypes.cast(value, CFStringRef),
+            buf,
+            1024,
+            kCFStringEncodingUTF8
+        )
+        if not result:
+            raise OSError('Error copying C string from CFStringRef')
+        string = buf.value
     if string is not None:
         string = string.decode('utf-8')
     return string
